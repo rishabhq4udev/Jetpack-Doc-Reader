65ae25675d252e89a5bfc17d90a3dc96a1a8fde4
Rishabh <rishabh@quantum4u.in>
2025-01-18 00:06:47 +0530

    first commit

---
+++ app/src/main/java/com/example/jetpackpdfreader/data/local/repository/DocRepositoryImpl.kt
@@ -0,0 +1,10 @@
+package com.example.jetpackpdfreader.data.local.repository
+
+import android.content.Context
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class DocRepositoryImpl @Inject constructor(private val context: Context) {
+
+}
